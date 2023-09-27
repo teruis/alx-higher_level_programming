ffc8c6514a13ccdc8664b0bbf375058d71410588
teruis <mohamadelassouli2002@gmail.com>
2023-09-27 13:43:03 +0100

    x

---
+++ 0x06-python-classes/5-main.py
@@ -0,0 +1,16 @@
+quare = __import__('5-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
