6170bb199d3e5cdb3b2c94985ae8521259286eb5
teruis <mohamadelassouli2002@gmail.com>
2023-12-11 20:40:09 +0100

    x

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
+// prints the first argument passed to it
 
-if (process.argv.length === 2) {
-    console.log("No argument");
-} else if (process.argv.length === 3) {
-    console.log("Argument found");
-} else {
-    console.log("Arguments found");
-}
+if (process.argv[2] === undefined) {
+    console.log('No argument');
+  } else {
+    console.log(process.argv[2]);
+  }
